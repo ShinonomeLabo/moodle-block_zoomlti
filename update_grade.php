fb91c88cc2c6a369ce3e1c6c4de750b749d1f4de
Takayuki Fuwa <fuwa@shinonomelabo.com>
2020-11-25 21:09:30 +0900

    文言調整

--- update_grade.php
+++ update_grade.php
@@ -35,63 +35,65 @@ $participants = $DB->get_records_sql($sql, ["instanceid" => $instanceid]);
 
 $service = new zoomlti_dao();
 $polls = $service->get_polls($instance->meeting_id);
-foreach ($polls->questions as $questions) {
-    $user = \core_user::get_user_by_email($questions->email);
-    if (!$user) {
-        continue;
-    }
-    $questions->question_details = array_reverse($questions->question_details);
-    foreach ($questions->question_details as $seq => $d) {
-        $score_passed = $DB->get_record("block_zoomlti_score_passed", ["zoomid" => $instanceid, "question_sequence" => $seq]);
 
-        $item = [];
-        $item['itemname'] = $d->question;
-        $item['categoryid'] = 1;
-        $item['gradetype'] = GRADE_TYPE_VALUE;
-        $item['grademax'] = 100;
-        $item['grademin'] = 0;
-        $item['gradepass'] = $score_passed->score_passed;
-
-        $scores = $DB->get_records("block_zoomlti_polls", ["zoomid" => $instanceid, "question_sequence" => $seq]);
-        $score_m = [];
-        foreach ($scores as $score) {
-            $score_m[$score->answer] = $score->score;
+foreach ($participants as $participant) {
+    foreach ($polls->questions as $questions) {
+        $user = \core_user::get_user($participant->userid);
+        if (!$user) {
+            continue;
         }
+        $questions->question_details = array_reverse($questions->question_details);
+        foreach ($questions->question_details as $seq => $d) {
+            $score_passed = $DB->get_record("block_zoomlti_score_passed", ["zoomid" => $instanceid, "question_sequence" => $seq]);
 
-        $user_answers = explode(";", $d->answer);
-        $u_score = 0;
-        foreach ($user_answers as $ua) {
-            $u_score += $score_m[$ua];
+            $item = [];
+            $item['itemname'] = $d->question;
+            $item['categoryid'] = 1;
+            $item['gradetype'] = GRADE_TYPE_VALUE;
+            $item['grademax'] = 100;
+            $item['grademin'] = 0;
+            $item['gradepass'] = $score_passed->score_passed;
+
+            $scores = $DB->get_records("block_zoomlti_polls", ["zoomid" => $instanceid, "question_sequence" => $seq]);
+            $score_m = [];
+            foreach ($scores as $score) {
+                $score_m[$score->answer] = $score->score;
+            }
+
+            $user_answers = explode(";", $d->answer);
+            $u_score = 0;
+            foreach ($user_answers as $ua) {
+                $u_score += $score_m[$ua];
+            }
+            $grade = grade_get_grades($zoom->course, 'mod', 'zoom', $zoom->id, $user->id);
+            $grade->userid = $user->id;
+            $grade->rawgrade = $u_score;
+
+            grade_update('mod/zoom', $courseid, 'mod', 'zoom', $instanceid, $seq, $grade, $item);
+
+            $user = \core_user::get_user($user->id);
+            $course = $DB->get_record("course", ["id" => $courseid]);
+
+            $event = event\zoomlti_meeting_polled::create([
+                'userid' => $user->id,
+                'objectid' => $instanceid,
+                'context' => $context,
+                'other' => [
+                    'topic' => $d->question,
+                    'source' => "moodle",
+                    'courseid' => $courseid,
+                    'username' => !$user ? null : $user->username,
+                    'moodleuserid' => !$user ? null : $user->id,
+                    'question_title' => $d->question,
+                    'question_answer' => $d->answer,
+                    'meeting_id' => $instance->meeting_id
+                ]
+            ]);
+
+            $event->add_record_snapshot('course', $course);
+            $event->add_record_snapshot("zoom", $instance);
+            $event->trigger();
         }
-        $grade = grade_get_grades($zoom->course, 'mod', 'zoom', $zoom->id, $user->id);
-        $grade->userid = $user->id;
-        $grade->rawgrade = $u_score;
-
-        grade_update('mod/zoom', $courseid, 'mod', 'zoom', $instanceid, $seq, $grade, $item);
-
-        $user = \core_user::get_user($user->id);
-        $course = $DB->get_record("course", ["id" => $courseid]);
-
-        $event = event\zoomlti_meeting_polled::create([
-            'userid' => $user->id,
-            'objectid' => $instanceid,
-            'context' => $context,
-            'other' => [
-                'topic' => $d->question,
-                'source' => "moodle",
-                'courseid' => $courseid,
-                'username' => !$user ? null : $user->username,
-                'moodleuserid' => !$user ? null : $user->id,
-                'question_title' => $d->question,
-                'question_answer' => $d->answer,
-                'meeting_id' => $instance->meeting_id
-            ]
-        ]);
-
-        $event->add_record_snapshot('course', $course);
-        $event->add_record_snapshot("zoom", $instance);
-        $event->trigger();
-
     }
 }
 
@@ -109,4 +111,4 @@ foreach ($participants as $participant) {
     grade_update('mod/zoom', $courseid, 'mod', 'zoom', $participant->zoomid, $participant->zoomid, $grade, $item);
 }
 
-redirect(new \moodle_url('index.php', ["courseid" => $courseid]), "成績の集計が完了しました。");
+//redirect(new \moodle_url('index.php', ["courseid" => $courseid]), "成績の集計が完了しました。");
